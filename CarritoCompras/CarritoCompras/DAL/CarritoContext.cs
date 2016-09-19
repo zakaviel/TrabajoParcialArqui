8b2292d8b70272cd986d596bbfc6afbad3ea034d
Aldo Alonso Rodirguez Hipolito <a_rodriguez_11@hotmail.com>
2016-09-19 17:19:29 -0500

    New CHanges
    Cambios a los Archivos DAL

---
+++ CarritoCompras/CarritoCompras/DAL/CarritoContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CarritoCompras.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace CarritoCompras.DAL
+{
+    public class CarritoContext : DbContext
+    {
+        public DbSet<Factura> Factura { get; set; }
+        public DbSet<Producto> Producto { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+           
+        }
+    }
+}
