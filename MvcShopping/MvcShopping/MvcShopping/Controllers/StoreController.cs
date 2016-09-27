6a95ae2175bef27e8dcb4fe601b09b928642dc9d
Aldo Alonso Rodirguez Hipolito <a_rodriguez_11@hotmail.com>
2016-09-26 22:20:20 -0500

    ProyectoActual
    Proyecto Actual

---
+++ MvcShopping/MvcShopping/MvcShopping/Controllers/StoreController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcShopping.Models;
+
+namespace MvcShopping.Controllers
+{
+    public class StoreController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+        // GET: Store
+        public ActionResult  Index()
+        {
+            var genres = storeDB.Genres.ToList();
+
+            return View(genres);
+        }
+        public ActionResult Browse(string genre)
+        {
+            //var genreModel = new Genre { Name = genre };
+            var genreModel = storeDB.Genres.Include("Albums")
+                .Single(g => g.Name == genre);
+            return View(genreModel);
+        }
+        public ActionResult Details(int id)
+        {
+            //var album = new Album { Title = "Album " + id };
+            var album = storeDB.Albums.Find(id);
+            return View(album);
+        }
+    }
+}
---
+++ MvcShopping/MvcShopping/MvcShopping/Models/MusicStoreEntities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MvcShopping.Models
+{
+    public class MusicStoreEntities : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        public System.Data.Entity.DbSet<MvcShopping.Models.Artist> Artists { get; set; }
+    }
+}
