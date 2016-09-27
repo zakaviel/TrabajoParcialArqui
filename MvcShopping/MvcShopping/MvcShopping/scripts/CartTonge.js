0f9612ceffc177aabc0c943aed5719a2ae6a17cf
Aldo Alonso Rodirguez Hipolito <a_rodriguez_11@hotmail.com>
2016-09-27 18:52:45 -0500

    Nuevo Commit
    Ez

---
+++ MvcShopping/MvcShopping/MvcShopping/scripts/CartTonge.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    var $
+
+        = $('#cart');
+    $('#clickme').click(function (e) {
+        e.stopPropagation();
+        if ($cart.is(":hidden")) {
+            $cart.slideDown("slow");
+        } else {
+            $cart.slideUp("slow");
+        }
+    });
+    $(document.body).click(function () {
+        if ($cart.not(":hidden")) {
+            $cart.slideUp("slow");
+        }
+    });
+});
